8e6837bd1127331f055af59896b93184d833cc27
20_h_thoreau <thoreaunc@gmail.com>
2021-09-06 20:20:46 -0400

    added completed background and meta tile system.

--- src/bg.c
+++ src/bg.c
@@ -5,12 +5,20 @@
 #include "cloudmap_bin.h"
 #include "backgroundpallete_bin.h"
 #include "defs.h"
+#define metatilexlen 0x4
+#define metatileylen 0x4
+#define mapxlen (0x20/metatilexlen)
+#define mapylen (0x40/metatileylen)
 
 void subtitlecloud1();
-const int blank[4][5]={{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}}
-const int cloud0[4][5]={{1,2,3,4,5},{6,7,7,8,9},{10,11,12,13,14},{0,0,0,0,0}};
-const int *tiles[2]={&blank,&cloud0}
+const u16 blank[metatileylen][metatilexlen]={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+const u16 cloud0[metatileylen][metatilexlen]={{1,2,3,4},{6,7,7,8},{10,11,12,13},{0,0,0,0}};
+const u16 *tiles[2]={&blank,&cloud0};
+const u16 bg2map[mapylen][mapxlen]={{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0},{1,0,1,0,1,0,1,0}};
+//4x4, meta tiles wide. 10 tall, set these correct after while
 
+void loadtile(u16 *mapptr, u16 metatilex, u16 metatiley, u16 tile);
+u32 scroll;
 void initbackground(){
     //REG_BGxCNT
     //REG_BGxHOFS
@@ -28,26 +36,46 @@ void initbackground(){
     REG_BG1HOFS=0;
     REG_BG2HOFS=0;
     REG_BG3HOFS=0;
+    
+    u32 scroll=0;
 
 }
 
+#define bg2 0x06001000
 void loadbackground(){
     tonccpy(BGPaletteMem,backgroundpallete_bin,backgroundpallete_bin_size);
     
     tonccpy(&Mem_Tile[2][0],clouds_bin,clouds_bin_size);
-    u16 *ptr=0x06001000;
     
-    for (int y=0; y<4; ++y){
-        for (int x=0; x<5; ++x){
-            *ptr=cloud1[y][x];
-            ++ptr;
+    for (u8 y=0; y<mapylen; ++y){
+        for(u8 x=0;x<mapxlen;++x){
+            loadtile(bg2,x,y,bg2map[y][x]);
+    
         }
-        ptr+=(32-5);
     }
+    
+
     //void subtilecloud1(ptr);
     
 }
-u32 scroll=0;
+
+void loadtile(u16 *mapptr, u16 metatilex, u16 metatiley, u16 tile){
+    metatilex*=mapxlen/2;
+    metatiley*=mapylen*mapxlen; //this should not be larger than 6 wide and 10 tall for the inputs
+    mapptr+=metatilex+metatiley;
+    u16 (*tileptr)[metatileylen][metatilexlen]=tiles[tile];
+    for (u8 y=0; y<metatileylen; ++y){
+        for (u8 x=0; x<metatilexlen; ++x){
+            *mapptr=(*tileptr)[y][x];
+            ++mapptr;
+            
+        }
+        mapptr+=(32-(metatilexlen));
+    }
+}
+//0 3e=x
+//0x40==y
+
 void scrollbackground(){
     ++scroll;
     REG_BG0VOFS=(scroll>>1)&0x1ff;
@@ -56,15 +84,4 @@ void scrollbackground(){
     
 }
 
-void subtitlecloud1(){
-    /*for (int y=0; y<4; ++y){
-        for (int x=0; x<5; ++x){
-            *ptr=1;
-            ptr+=2;
-        }
-        ptr+=(64-10);
-    }*/
-    int *mapptr=0x06000802;
-    mapptr=2;
-    
-}
+
